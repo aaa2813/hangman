02bd4578d29a351392f8ac13139c2e3217f5e32d
Andy Athawale <andy.athawale@gmail.com>
2024-11-24 21:57:42 -0600

    Removed unused helper function file.

--- getlist.py
+++
@@ -1,8 +0,0 @@
-with open("movies.txt") as f:
-    for line in f:
-        l = line.rstrip()
-        try:
-            cleaned = l[:l.index('-')-1]
-            print(cleaned)
-        except:
-            print("nothing to show here")
